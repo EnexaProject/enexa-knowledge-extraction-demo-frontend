9bfba2dd20ff12798588ad951229e7c4fd6448d3
Michael Röder <michael.roeder@uni-paderborn.de>
2023-08-22 13:18:23 +0200

    Updated the download script for demo modules.

--- download-module-files.sh
+++ download-module-files.sh
@@ -2,14 +2,14 @@
 # Extraction module
 curl -o modules/extraction.ttl https://raw.githubusercontent.com/EnexaProject/enexa-extraction-module/main/module.ttl
 # KG Repair module
-# TODO
+curl -o modules/repair.ttl https://raw.githubusercontent.com/EnexaProject/enexa-kg-fixing-module/master/module.ttl
 # Transformation module
 curl -o modules/transform.ttl https://raw.githubusercontent.com/EnexaProject/enexa-transform-module/main/module.ttl
 # Tentris service
-# TODO
+# TODO curl -o modules/tentris.ttl 
 # DICE Embeddings module
 curl -o modules/embeddings.ttl https://raw.githubusercontent.com/EnexaProject/enexa-dice-embeddings/master/module.ttl
 # CEL Train module
-# TODO
+# TODO curl -o modules/cel-train.ttl 
 # CEL service
-# TODO
+# TODO curl -o modules/cel-service.ttl 
