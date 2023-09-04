f76c26dac873188dded49017bacdebe311814085
Michael Röder <michael.roeder@uni-paderborn.de>
2023-09-04 18:51:24 +0200

    Added wikidata preprocess module.

--- download-module-files.sh
+++ download-module-files.sh
@@ -3,6 +3,8 @@
 curl -o modules/extraction.ttl https://raw.githubusercontent.com/EnexaProject/enexa-extraction-module/main/module.ttl
 # KG Repair module
 curl -o modules/repair.ttl https://raw.githubusercontent.com/EnexaProject/enexa-kg-fixing-module/master/module.ttl
+# Wikidata preprocessing module
+curl -o modules/wikidata-preproc.ttl https://raw.githubusercontent.com/EnexaProject/enexa-wikidata-preprocess-module/main/module.ttl
 # Transformation module
 curl -o modules/transform.ttl https://raw.githubusercontent.com/EnexaProject/enexa-transform-module/main/module.ttl
 # Tentris service
